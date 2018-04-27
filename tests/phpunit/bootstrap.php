64f34281b563b62497cb67b3d44607511b83cacc
Akolzin Dmitry <akolzinvs@gmail.com>
2018-04-27 11:47:09 +0300

    Fix bootstrap

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -9,19 +9,19 @@ if (!$_tests_dir) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-    $plugin_dir = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/';
+    $plugin_dir =  dirname( dirname( dirname( __FILE__ ) ) )  . '/';
 
-    require $plugin_dir . 'woocommerce-retailcrm/src/include/class-wc-retailcrm-orders.php';
-    require $plugin_dir . 'woocommerce-retailcrm/src/include/class-wc-retailcrm-customers.php';
-    require $plugin_dir . 'woocommerce-retailcrm/src/include/class-wc-retailcrm-inventories.php';
-    require $plugin_dir . 'woocommerce-retailcrm/src/retailcrm.php';
+    require $plugin_dir . 'src/include/class-wc-retailcrm-orders.php';
+    require $plugin_dir . 'src/include/class-wc-retailcrm-customers.php';
+    require $plugin_dir . 'src/include/class-wc-retailcrm-inventories.php';
+    require $plugin_dir . 'src/retailcrm.php';
 }
 
 tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
 require '/tmp/woocommerce/tests/bootstrap.php';
 
-$plugin_dir = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/';
+$plugin_dir = dirname( dirname( dirname( __FILE__ ) ) ) . '/';
 // helpers
-require $plugin_dir . 'woocommerce-retailcrm/tests/helpers/class-wc-retailcrm-response-helper.php';
-require $plugin_dir . 'woocommerce-retailcrm/tests/helpers/class-wc-retailcrm-test-case-helper.php';
+require $plugin_dir . 'tests/helpers/class-wc-retailcrm-response-helper.php';
+require $plugin_dir . 'tests/helpers/class-wc-retailcrm-test-case-helper.php';
