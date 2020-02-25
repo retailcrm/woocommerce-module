147ebb02abf22c7bf0576b40995aa91dadd12d7d
Akolzin Dmitry <akolzinvs@gmail.com>
2020-02-25 14:45:04 +0300

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ if (!$_tests_dir) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-    $plugin_dir =  dirname(dirname(dirname(__FILE__))) . '/';
+    $plugin_dir =  dirname(dirname(__FILE__)) . '/';
 
     require $plugin_dir . 'src/include/class-wc-retailcrm-orders.php';
     require $plugin_dir . 'src/include/class-wc-retailcrm-customers.php';
