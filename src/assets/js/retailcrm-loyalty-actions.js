0f511adbba12d807a3beffa09f75514a543830e3
Kocmonavtik <61938582+Kocmonavtik@users.noreply.github.com>
2024-04-04 09:10:34 +0300

    ref #71905 Add API methods for loyalty program (#324)
    rebase
    
    rebase

---
+++ src/assets/js/retailcrm-loyalty-actions.js
@@ -0,0 +1,121 @@
+jQuery(function() {
+    jQuery('#loyaltyRegisterForm').on("submit", function (event) {
+        var termsCheck = jQuery('#termsLoyalty');
+        var privacyCheck = jQuery('#privacyLoyalty');
+
+        if (termsCheck.length) {
+            if (!termsCheck.is(':checked')) {
+                event.preventDefault();
+                return false;
+            }
+        }
+
+        if (privacyCheck.length) {
+            if (!privacyCheck.is(':checked')) {
+                event.preventDefault();
+                return false;
+            }
+        }
+
+        let phone = jQuery('#phoneLoyalty');
+
+        if (!phone.val().match(/(?:\+|\d)[\d\-\(\) ]{7,}\d/)) {
+
+            if (!jQuery('#warningLoyaltyPhone').length) {
+                phone.parent().append('<span style="color: red" id="warningLoyaltyPhone">' + messagePhone + '</span>')
+            }
+
+            event.preventDefault();
+            return false;
+        } else {
+            jQuery('#warningLoyaltyPhone').remove();
+        }
+
+        jQuery.ajax({
+            url: loyaltyUrl.url + '/admin-ajax.php?action=register_customer_loyalty',
+            method: 'POST',
+            timeout: 0,
+            data: {ajax: 1, phone: phone.val(), userId: customerId},
+            dataType: 'json'
+        })
+            .done(function (response) {
+                if (response.hasOwnProperty('error')) {
+                    jQuery('#loyaltyRegisterForm').append('<p style="color: red">'+ response.error + '</p>')
+
+                    event.preventDefault();
+                    return false;
+                } else {
+                    location.reload();
+                }
+            })
+
+        event.preventDefault();
+    });
+
+    jQuery('#loyaltyActivateForm').on("submit", function (event) {
+        var activateCheckbox = jQuery('#loyaltyActiveCheckbox');
+
+        if (activateCheckbox.length) {
+            if (!activateCheckbox.is(':checked')) {
+                event.preventDefault();
+                return false;
+            }
+        }
+
+        jQuery.ajax({
+            url: loyaltyUrl.url + '/admin-ajax.php?action=activate_customer_loyalty',
+            method: 'POST',
+            timeout: 0,
+            data: {ajax: 1, loyaltyId: loyaltyId},
+            dataType: 'json'
+        })
+            .done(function (response) {
+                if (response.hasOwnProperty('error')) {
+                    jQuery('#loyaltyRegisterForm').append('<p style="color: red">'+ response.error + '</p>')
+
+                    event.preventDefault();
+                    return false;
+                } else {
+                    location.reload();
+                }
+            })
+
+        event.preventDefault();
+    });
+
+    jQuery('.popup-open-loyalty').click(function() {
+        if (jQuery(this).attr('id') === 'terms-popup') {
+            jQuery('#popup-loyalty-text').html(termsLoyalty);
+        } else {
+            jQuery('#popup-loyalty-text').html(privacyLoyalty);
+        }
+
+        jQuery('.popup-fade-loyalty').fadeIn();
+        return false;
+    });
+
+    jQuery('.popup-close-loyalty').click(function() {
+        jQuery(this).parents('.popup-fade-loyalty').fadeOut();
+        return false;
+    });
+
+    jQuery(document).keydown(function(e) {
+        if (e.keyCode === 27) { // Key Escape
+            e.stopPropagation();
+            jQuery('.popup-fade-loyalty').fadeOut();
+        }
+    });
+
+    jQuery('.popup-fade-loyalty').click(function(e) {
+        if (jQuery(e.target).closest('.popup-loyalty').length == 0) {
+            jQuery(this).fadeOut();
+        }
+    });
+
+    jQuery('#phoneLoyalty').keydown(function (e) {
+        let key = e.key;
+
+        return (key >= '0' && key <= '9') || key == '+' || key == '(' || key == ')'|| key == '-' ||
+            key == 'ArrowLeft' || key == 'ArrowRight' || key == 'Delete' || key == 'Backspace';
+    });
+});
---
+++ tests/test-wc-retailcrm-loyalty.php
@@ -0,0 +1,92 @@
+<?php
+
+use datasets\DataLoyaltyRetailCrm;
+
+if (!class_exists('WC_Retailcrm_Loyalty')) {
+    include_once(WC_Integration_Retailcrm::checkCustomFile('class-wc-retailcrm-loyalty.php'));
+}
+
+if (!class_exists('WC_Retailcrm_Response')) {
+    include_once(WC_Integration_Retailcrm::checkCustomFile('include/api/class-wc-retailcrm-response.php'));
+}
+
+
+/**
+ * PHP version 7.0
+ *
+ * Class WC_Retailcrm_Loyalty_Test
+ *
+ * @category Integration
+ * @author   RetailCRM <integration@retailcrm.ru>
+ * @license  http://retailcrm.ru Proprietary
+ * @link     http://retailcrm.ru
+ * @see      http://help.retailcrm.ru
+ */
+class WC_Retailcrm_Loyalty_Test extends WC_Retailcrm_Test_Case_Helper
+{
+    protected $responseMock;
+    protected $apiMock;
+
+    /** @var \WC_Retailcrm_Loyalty */
+    protected $loyalty;
+
+    public function setUp()
+    {
+        $this->responseMock = $this->getMockBuilder('\WC_Retailcrm_Response_Helper')
+            ->disableOriginalConstructor()
+            ->setMethods(['isSuccessful'])
+            ->getMock()
+        ;
+
+        $this->responseMock->setResponse(['success' => true]);
+        $this->setMockResponse($this->responseMock, 'isSuccessful', true);
+
+        $this->apiMock = $this->getMockBuilder('\WC_Retailcrm_Client_V5')
+            ->disableOriginalConstructor()
+            ->setMethods(['customersGet', 'getLoyaltyAccountList', 'createLoyaltyAccount', 'activateLoyaltyAccount'])
+            ->getMock()
+        ;
+
+        $this->setMockResponse($this->apiMock, 'customersGet', ['customer' => ['id' => 1]]);
+        $this->setMockResponse($this->apiMock, 'createLoyaltyAccount', $this->responseMock);
+        $this->setMockResponse($this->apiMock, 'activateLoyaltyAccount', $this->responseMock);
+
+        $this->loyalty = new WC_Retailcrm_Loyalty($this->apiMock, []);
+    }
+
+    /**
+     * @dataProvider responseLoyalty
+     */
+    public function testGetForm($isSuccessful, $body, $expected)
+    {
+        $response = new WC_Retailcrm_Response($isSuccessful ? 200 : 400, $body);
+
+        $this->setMockResponse($this->apiMock, 'getLoyaltyAccountList', $response);
+        $this->loyalty = new WC_Retailcrm_Loyalty($this->apiMock, []);
+
+        $result = $this->loyalty->getForm(1);
+
+        if (isset($result['form'])) {
+            $this->assertTrue((bool) stripos($result['form'], $expected));
+        }
+    }
+
+    public function testRegistrationLoyalty()
+    {
+        $result = $this->loyalty->registerCustomer(1, '89999999999', 'test');
+
+        $this->assertTrue($result);
+    }
+
+    public function testActivateLoyalty()
+    {
+        $result = $this->loyalty->activateLoyaltyCustomer(1);
+
+        $this->assertTrue($result);
+    }
+
+    public function responseLoyalty()
+    {
+        return DataLoyaltyRetailCrm::getDataLoyalty();
+    }
+}
