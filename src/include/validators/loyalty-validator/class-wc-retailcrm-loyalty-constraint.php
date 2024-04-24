80399b661ff7389f8918ababfb8734c569065677
Ivan Chaplygin <chaplygin@retailcrm.ru>
2024-04-24 10:40:43 +0300

    ref #95034 Adding classes for validation

---
+++ src/include/validators/loyalty-validator/class-wc-retailcrm-loyalty-constraint.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!class_exists('WC_Retailcrm_Loyalty_Constraint')) :
+    /**
+     * PHP version 7.0
+     *
+     * Class WC_Retailcrm_Loyalty_Constraint - Constraint for CRM loyalty.
+     *
+     * @category Integration
+     * @author   RetailCRM <integration@retailcrm.ru>
+     * @license  http://retailcrm.ru Proprietary
+     * @link     http://retailcrm.ru
+     * @see      http://help.retailcrm.ru
+     */
+    class WC_Retailcrm_Loyalty_Constraint
+    {
+        /** @var string  */
+        public $err = 'error';
+    }
+endif;
---
+++ src/include/validators/loyalty-validator/class-wc-retailcrm-loyalty-validator.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!class_exists('WC_Retailcrm_Loyalty_Validator')) :
+    /**
+     * PHP version 7.0
+     *
+     * Class WC_Retailcrm_Loyalty_Validator - Validate CRM loyalty.
+     *
+     * @category Integration
+     * @author   RetailCRM <integration@retailcrm.ru>
+     * @license  http://retailcrm.ru Proprietary
+     * @link     http://retailcrm.ru
+     * @see      http://help.retailcrm.ru
+     */
+    class WC_Retailcrm_Loyalty_Validator extends WC_Retailcrm_Loyalty_Constraint
+    {
+        public function checkAccount(int $userId)
+        {
+
+        }
+    }
+endif;
