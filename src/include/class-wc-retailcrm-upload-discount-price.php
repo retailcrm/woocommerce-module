e083f3eb4d4205b789ec698deb06b18356d2bbcd
Ivan Chaplygin <chaplygin@retailcrm.ru>
2024-08-30 18:04:06 +0300

    Added transfer of the base price when generating a catalog with an enabled loyalty program Creating a script for transferring a promotional price.

---
+++ src/include/class-wc-retailcrm-upload-discount-price.php
@@ -0,0 +1,205 @@
+<?php
+
+if (!class_exists('WC_Retailcrm_Upload_Discount_Price')):
+
+    class WC_Retailcrm_Upload_Discount_Price
+    {
+        const DISCOUNT_TYPE_PRICE = 'woo-promotion-lp';
+
+        protected $activeLoyalty = false;
+
+        protected $settings;
+
+        protected $site;
+
+        /** @var bool|WC_Retailcrm_Proxy|WC_Retailcrm_Client_V5 */
+        protected $apiClient;
+
+        public function __construct($aplClient = false)
+        {
+            $this->settings = get_option(WC_Retailcrm_Base::$option_key);
+            $this->apiClient = $aplClient;
+
+            if (isset($this->settings['loyalty']) && $this->settings['loyalty'] === WC_Retailcrm_Base::YES) {
+                $this->activeLoyalty = true;
+            }
+        }
+
+        public function upload()
+        {
+            $error = $this->uploadSettings();
+
+            if ($error !== '') {
+                writeBaseLogs($error);
+            }
+
+            $productStatuses = $this->getProductStatuses();
+
+            if (!$productStatuses) {
+                $productStatuses = ['publish'];
+            }
+
+            $page = 1;
+            $requestData = [];
+            $counter = 0;
+
+            do {
+                $products = wc_get_products(
+                    [
+                        'limit' => 1000,
+                        'status' => $productStatuses,
+                        'page' => $page,
+                        'paginate' => true
+                    ]
+                );
+
+                wp_cache_flush_runtime();
+
+                if (empty($products)) {
+                    writeBaseLogs('Can`t get products!');
+
+                    return;
+                }
+
+                try {
+                    foreach ($products->products as $offer) {
+                        $type = $offer->get_type();
+
+                        if (strpos($type, 'variable') !== false || strpos($type, 'variation') !== false) {
+                            foreach ($offer->get_children() as $childId) {
+                                $childProduct = wc_get_product($childId);
+
+                                if (!$childProduct) {
+                                    continue;
+                                }
+
+                                $requestData[] = $this->getOfferData($childProduct);
+                                ++$counter;
+                            }
+                        } else {
+                            $requestData[] = $this->getOfferData($offer);
+                            ++$counter;
+                        }
+                    }
+
+                    if ($counter > 250) {
+                        $chunks = array_chunk($requestData, 250);
+                        $lastKeyChunk = key(end($chunks));
+
+
+                        if ($lastKeyChunk !== 0) {
+                            $requestData = $chunks[$lastKeyChunk];
+                            $counter = count($requestData);
+
+                            unset($chunks[$lastKeyChunk]);
+                        }
+
+                        foreach ($chunks as $chunk) {
+                            $this->apiClient->storePricesUpload($chunk, $this->site);
+                        }
+                    }
+                } catch (\Throwable $exception) {
+                    writeBaseLogs($exception->getMessage() . PHP_EOL . $exception->getTraceAsString());
+
+                    return;
+                }
+            } while ($page <= $products->max_num_pages);
+        }
+
+        private function getOfferData(WC_Product $product)
+        {
+            return [
+                'externalId' => $product->get_id(),
+                'site' => $this->site,
+                'prices' => [
+                    'code' => self::DISCOUNT_TYPE_PRICE,
+                    'price' => wc_get_price_including_tax($product)
+                ]
+            ];
+        }
+
+        private function getProductStatuses()
+        {
+            $statuses = [];
+
+            foreach (get_post_statuses() as $key => $value) {
+                if (isset($this->settings['p_' . $key]) && $this->settings['p_' . $key] == WC_Retailcrm_Base::YES) {
+                    $statuses[] = $key;
+                }
+            }
+
+            return $statuses;
+        }
+
+        private function uploadSettings()
+        {
+            if (!$this->apiClient instanceof WC_Retailcrm_Proxy) {
+               return 'API client has not been initialized';
+            }
+
+            $this->site = $this->apiClient->getSingleSiteForKey();
+
+            if (empty($this->site)) {
+                return 'Error with CRM credentials: need an valid apiKey assigned to one certain site';
+            }
+
+            $response = $this->apiClient->getPriceTypes();
+
+            if (
+                !$response instanceof WC_Retailcrm_Response
+                || !$response->offsetExists('priceTypes')
+                || empty($response['priceTypes'])
+            ) {
+                return 'Error getting price types';
+            }
+
+            $defaultPrice = null;
+            $discountPriceType = null;
+
+            foreach ($response['priceTypes'] as $priceType) {
+                if ($priceType['default'] === true) {
+                    $defaultPrice = $priceType;
+                }
+
+                if ($priceType['code'] === self::DISCOUNT_TYPE_PRICE) {
+                    $discountPriceType = $priceType;
+                }
+            }
+
+            if ($discountPriceType === null) {
+                $discountPriceType = [
+                    'code' => self::DISCOUNT_TYPE_PRICE,
+                    'name' => 'Акционная цена Woocommerce',
+                    'active' => true,
+                    'description' => 'Акционный тип цены для магазина Woocommerce, сгенерированный автоматически. Необходим для корректной работы синхронизации при включенной программы лояльности (Не удалять. Не деактивировать)',
+                    'ordering' => 999
+                ];
+
+                if (isset($defaultPrice['geo'])) {
+                    $discountPriceType['geo'] = $defaultPrice['geo'];
+                }
+
+                if (isset($defaultPrice['groups'])) {
+                    $discountPriceType['groups'] = $defaultPrice['groups'];
+                }
+
+                if (isset($defaultPrice['currency'])) {
+                    $discountPriceType['currency'] = $defaultPrice['currency'];
+                }
+
+                if (isset($defaultPrice['filterExpression'])) {
+                    $discountPriceType['filterExpression'] = $defaultPrice['filterExpression'];
+                }
+
+                $response = $this->apiClient->editPriceType($discountPriceType);
+
+                if (!$response instanceof WC_Retailcrm_Response || !$response['success']) {
+                    return 'Error creating price type';
+                }
+            }
+
+            return '';
+        }
+    }
+
+endif;
