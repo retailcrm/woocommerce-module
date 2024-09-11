e25e383aff6542fffe0402deddf38825bc385ee1
Ivan Chaplygin <chaplygin@retailcrm.ru>
2024-09-11 16:22:18 +0300

    Added script for entering a coupon on click

---
+++ src/assets/js/retailcrm-loyalty-cart.js
@@ -0,0 +1,6 @@
+function inputLoyaltyCode() {
+    let couponInput = document.getElementById('coupon_code');
+    let couponCode = document.getElementById('input_loyalty_code');
+
+    couponInput.value = couponCode.innerText;
+}
