fec5f24f615688dff9fa7dd64c1756e3cf55cc1b
Ivan Chaplygin <chaplygin@retailcrm.ru>
2024-09-18 09:59:33 +0300

    Add link documentation Loyalty program

--- src/include/abstracts/class-wc-retailcrm-abstracts-settings.php
+++ src/include/abstracts/class-wc-retailcrm-abstracts-settings.php
@@ -629,7 +629,9 @@ abstract class WC_Retailcrm_Abstracts_Settings extends WC_Integration
                     'title'       => __('Loyalty program', 'retailcrm'),
                     'class'       => 'checkbox',
                     'type'        => 'checkbox',
-                    'description' => '<b style="color: red">' . __('Attention! When activating the loyalty program, the method of ICML catalog generation changes. Details in documentation', 'retailcrm') . '<\b>'
+                    'description' => '<b style="color: red">' .
+                     __('Attention! When activating the loyalty program, the method of ICML catalog generation changes. Details in', 'retailcrm') .
+                      ' <\b>' . __("<a href='https://docs.simla.com/Users/Integration/SiteModules/WooCommerce/PLWoocommerce'>documentation loyalty program</a>", "retailcrm")
                 ];
 
                 $this->form_fields['loyalty_terms'] = [
