b5bbfd888d541acc0d08d1b92bbb0b5380961da1
Alexandr <polupanov@retailcrm.ru>
2026-03-24 10:16:06 +0300

    Update version in woo-retailcrm.php

--- src/woo-retailcrm.php
+++ src/woo-retailcrm.php
@@ -30,7 +30,7 @@ if (!class_exists( 'WC_Integration_Retailcrm')) :
     class WC_Integration_Retailcrm {
         const WOOCOMMERCE_SLUG = 'woocommerce';
         const WOOCOMMERCE_PLUGIN_PATH = 'woocommerce/woocommerce.php';
-        const MODULE_VERSION = '5.0.12';
+        const MODULE_VERSION = '5.0.13';
 
         private static $instance;
 
